5ca508e45bc3b42d2cf4ddb82d4b987a9fb9442b
UweStolz <uwe.stolz@prodexa.com>
2020-12-04 10:14:20 +0100

    fix: linter errors

--- request.ts
+++ request.ts
@@ -1,16 +1,21 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import axios, { AxiosRequestConfig, AxiosResponse } from 'axios';
 
 export async function GET(url: string, options: AxiosRequestConfig | undefined): Promise<any|null> {
-    let data = null;
-    let response: AxiosResponse<any>|null = null;
-    try {
-        response = await axios.get(url, options)
-    } catch(err) {
-        // Ignore error
-        console.error(err);
-    }
-    if (response?.status == 200) {
-        data = response.data;
-    }
-    return data;
+  let data = null;
+  let response: AxiosResponse<any>|null = null;
+  try {
+    response = await axios.get(url, options);
+  } catch (err) {
+    // Ignore error
+    console.error(err);
+  }
+  if (response?.status === 200) {
+    data = response.data;
+  }
+  return data;
+}
+
+export async function POST(url: string, options: AxiosRequestConfig | undefined): Promise<any | null> {
+  //
 }
