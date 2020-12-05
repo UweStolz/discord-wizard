1107268e209f4f7674140cc11105690654069111
UweStolz <uwe.stolz@prodexa.com>
2020-12-05 03:21:45 +0100

    ref: add obj logger

--- logger.ts
+++ logger.ts
@@ -10,4 +10,15 @@ const logger = winston.createLogger({
   ],
 });
 
+const objLogger = winston.createLogger({
+  format: winston.format.combine(
+    winston.format.prettyPrint({ colorize: true }),
+  ),
+  transports: [
+    new winston.transports.Console(),
+  ],
+});
+
 export default logger;
+
+export { objLogger };
