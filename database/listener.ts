5d359cf666725f5968ade11e73ca83c2eb20f47e
UweStolz <uwe.stolz@prodexa.com>
2020-12-05 16:58:30 +0100

    ref: db adjustments

--- database/listener.ts
+++ database/listener.ts
@@ -12,11 +12,18 @@ export default function listener(pool: Pool): void {
   });
   pool.on('error', (err: Error, client: PoolClient) => {
     logger.error('An error ocurred in the DB');
-    objLogger.error(`Client: ${client}`);
+    objLogger.debug(client);
     objLogger.error(err);
   });
   pool.on('remove', (client: PoolClient) => {
     logger.info('Database closed and removed client');
     objLogger.debug(client);
+  });
+}
+
+export function clientListener(client: PoolClient): void {
+  client.on('error', (err: Error) => {
+    logger.error('An error ocurred in the DB');
+    objLogger.error(err);
   });
 }
