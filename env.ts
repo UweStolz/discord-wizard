2aad7a19a1275d2e78cf66ae4a59f2513ea91ab3
UweStolz <uwe.stolz@prodexa.com>
2020-12-05 15:03:25 +0100

    feat: create database if it doesnt exist

--- env.ts
+++ env.ts
@@ -3,19 +3,11 @@ import 'dotenv/config';
 const token = process.env.TOKEN;
 const owlBotToken = process.env.OWLBOT;
 const serverId = process.env.SERVERID;
-
-// Database
-const db = {
-  dataBaseUrl: process.env.DATABASE_URL,
-  database: process.env.database,
-  user: process.env.user,
-  port: process.env.port,
-  password: process.env.password,
-};
+const databaseUrl = process.env.DATABASE_URL;
 
 export {
   token,
   owlBotToken,
   serverId,
-  db,
+  databaseUrl,
 };
