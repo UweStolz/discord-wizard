39f253eba608dec0fcdba83a9533574eb37f25e2
UweStolz <uwe.stolz@protonmail.com>
2020-12-07 14:59:55 +0100

    ref: generalize handler

--- handler.ts
+++ handler.ts
@@ -4,7 +4,7 @@ import { publicApis, env } from './data';
 import request from './request';
 import logger from './logger';
 
-export async function stats(message: Discord.Message): Promise<void> {
+async function stats(message: Discord.Message): Promise<void> {
   const statistics: string = await utils.dbHelper.getStatistics();
   const embed = new Discord.MessageEmbed({
     title: 'Statistics',
@@ -13,7 +13,7 @@ export async function stats(message: Discord.Message): Promise<void> {
   await message.channel.send(embed);
 }
 
-export async function help(message: Discord.Message): Promise<void> {
+async function help(message: Discord.Message): Promise<void> {
   const helpMessage = utils.helper.getHelpMessage();
   const embed = new Discord.MessageEmbed({
     title: 'Helpful help, that really helps...probably',
@@ -22,33 +22,33 @@ export async function help(message: Discord.Message): Promise<void> {
   await message.channel.send(embed);
 }
 
-export async function ping(message: Discord.Message): Promise<void> {
+async function ping(message: Discord.Message): Promise<void> {
   await message.channel.send('pong');
   await utils.dbHelper.updateStatForColumn('ping');
 }
 
-export async function catFact(message: Discord.Message): Promise<void> {
+async function catFact(message: Discord.Message): Promise<void> {
   const { fact } = await request('GET', publicApis.catFact, undefined);
   if (fact) {
     await message.channel.send(`FACT: "${fact}"`);
   }
 }
 
-export async function catPic(message: Discord.Message): Promise<void> {
+async function catPic(message: Discord.Message): Promise<void> {
   const { file } = await request('GET', publicApis.catPic, undefined);
   if (file) {
     await message.channel.send(file);
   }
 }
 
-export async function quote(message: Discord.Message): Promise<void> {
+async function quote(message: Discord.Message): Promise<void> {
   const requestedQuote = await request('GET', publicApis.quotes[0], undefined);
   if (requestedQuote.length > 0) {
     await message.channel.send(`Quote: "${requestedQuote[0]}"`);
   }
 }
 
-export async function insult(message: Discord.Message, client: Discord.Client, argument: string|null = null): Promise<void> {
+async function insult(message: Discord.Message, client: Discord.Client, argument: string|null = null): Promise<void> {
   const insultToMember = await request('GET', publicApis.insult, undefined) || null;
   const allMembers = utils.discordHelper.getMemberFromServer(client);
 
@@ -76,14 +76,14 @@ export async function insult(message: Discord.Message, client: Discord.Client, a
   }
 }
 
-export async function bored(message: Discord.Message): Promise<void> {
+async function bored(message: Discord.Message): Promise<void> {
   const { activity } = await request('GET', publicApis.bored, undefined) || null;
   if (activity) {
     await message.channel.send(`How about..? - ${activity}`);
   }
 }
 
-export async function whatIs(message: Discord.Message, argument: string|null = null): Promise<void> {
+async function whatIs(message: Discord.Message, argument: string|null = null): Promise<void> {
   const owlBotResponse: owlbotResponse = await request(
     'GET',
     `${publicApis.owlbot}${argument}`,
@@ -118,11 +118,31 @@ export async function defaultHandler(message: Discord.Message): Promise<void> {
   await message.channel.send(defaultMessage);
 }
 
-// TODO
-const handlerMapping = {
-  stats: async (message: Discord.Message) => stats(message),
+interface Handlers {
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  [name: string]: (message: Discord.Message, ...args: any[]) => Promise<void>;
+}
+
+const handlerMapping: Handlers = {
+  stats,
+  help,
+  ping,
+  catFact,
+  catPic,
+  quote,
+  insult,
+  bored,
+  whatIs,
 };
 
-export default async function handleMessage(command: Command, message: Discord.Message) {
-  await utils.dbHelper.updateStatForColumn('ping');
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+export default async function handleCommand(command: Command, message: Discord.Message, ...args: any[]): Promise<void> {
+  const func = handlerMapping[command];
+  if (func) {
+    await func(message, args);
+    const cleanedCommand = utils.helper.globallyReplaceDashWithUnderscore(command);
+    await utils.dbHelper.updateStatForColumn(cleanedCommand);
+  } else {
+    defaultHandler(message);
+  }
 }
